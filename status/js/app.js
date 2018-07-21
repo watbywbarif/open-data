15fc3c3f7c11e1ab9d86a6b0b8faae84796cb924
Miroslav Schlossberg <mschlos@gmail.com>
2018-07-21 16:56:52 +0200

    update loading

--- status/js/app.js
+++ status/js/app.js
@@ -1,7 +1,7 @@
 jQuery(function($) {
   var url = 'https://docs.google.com/spreadsheet/ccc?key=12U19YuA1q4CkHx0qMl3KSrweAXsXxfUAJqUHhXWPeoo#gid=0';
   var $el = $('.load-status');
-  $el.html('<h3>Učitavanje <img src="https://assets.okfn.org/images/icons/ajaxload-circle.gif" /></h3>');
+  $el.html('<div class="loader"></div>');
   var tmpl = $('#our-template').html();
   recline.Backend.GDocs.fetch({url: url})
     .done(function(results) {
